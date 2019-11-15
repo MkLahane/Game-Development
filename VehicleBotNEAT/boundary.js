4cd4ae77d877783a87100ebecdf43389fef4a38c
Makarand Lahane <jayhoylehawkings@gmail.com>
2019-11-15 07:41:32 -0500

    Update boundary.js

--- VehicleBotNEAT/boundary.js
+++ VehicleBotNEAT/boundary.js
@@ -6,14 +6,16 @@ class Boundary {
     setB(x, y) {
       this.b.set(x, y);
     }
-    show() {
-      stroke(255);
+    show(col) {
+      stroke(col);
       noFill();
       strokeWeight(4);
       line(this.a.x, this.a.y, this.b.x, this.b.y);
+      
     }
     getMidPoint() {
       return createVector((this.a.x + this.b.x) * 0.5, (this.a.y + this.b.y) * 0.5);
     }
 }
   
+
